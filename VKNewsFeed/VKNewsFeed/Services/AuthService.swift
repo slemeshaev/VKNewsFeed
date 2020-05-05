6e35055d6f3197d05652f8bb0be6b5d3409a777b
Stanislav <lemeshaevs@gmail.com>
2020-05-05 22:34:51 +0300

    Создал файл AuthService

---
+++ VKNewsFeed/VKNewsFeed/Services/AuthService.swift
@@ -0,0 +1,9 @@
+//
+//  AuthService.swift
+//  VKNewsFeed
+//
+//  Created by Станислав Лемешаев on 05.05.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
