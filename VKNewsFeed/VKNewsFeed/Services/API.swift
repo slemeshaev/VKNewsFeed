a6b53e33f8dbf56de986bade08c9b2f441c834c2
Stanislav <lemeshaevs@gmail.com>
2020-05-14 22:54:47 +0300

    Создал структуру API и сетевой сервис

---
+++ VKNewsFeed/VKNewsFeed/Services/API.swift
@@ -0,0 +1,9 @@
+//
+//  API.swift
+//  VKNewsFeed
+//
+//  Created by Станислав Лемешаев on 14.05.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
---
+++ VKNewsFeed/VKNewsFeed/Services/NetworkService.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkService.swift
+//  VKNewsFeed
+//
+//  Created by Станислав Лемешаев on 14.05.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
