352990195774ee9112edf2a5474c64c35cc9d069
Stanislav <lemeshaevs@gmail.com>
2020-05-14 22:57:33 +0300

    VK SDK API Networking Logic

--- VKNewsFeed/VKNewsFeed/FeedNews/FeedViewController.swift
+++ VKNewsFeed/VKNewsFeed/FeedNews/FeedViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class FeedViewController: UIViewController {
 
+    private let networkService = NetworkService()
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .green
+        networkService.getFeed()
     }
     
 
--- VKNewsFeed/VKNewsFeed/Services/API.swift
+++ VKNewsFeed/VKNewsFeed/Services/API.swift
@@ -7,3 +7,11 @@
 //
 
 import Foundation
+
+struct API {
+    static let scheme = "https"
+    static let host = "api.vk.com"
+    static let version = "5.103"
+    
+    static let newsFeed = "/method/newsfeed.get"
+}
