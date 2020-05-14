5d6f3430fe2cc29cd81248b61cdf90c34efbb69d
Stanislav <lemeshaevs@gmail.com>
2020-05-14 23:17:56 +0300

    Fetch News Feed JSON

--- VKNewsFeed/VKNewsFeed/Services/NetworkService.swift
+++ VKNewsFeed/VKNewsFeed/Services/NetworkService.swift
@@ -8,7 +8,11 @@
 
 import Foundation
 
-final class NetworkService {
+protocol Networking {
+    func request(path: String, params: [String: String], completion: @escaping (Data?, Error?) -> ())
+}
+
+final class NetworkService: Networking {
     
     private let authService: AuthService
     
@@ -16,23 +20,35 @@ final class NetworkService {
         self.authService = authService
     }
     
-    // получение новостей
-    func getFeed() {
-        var components = URLComponents()
-        
+    // функция запроса
+    func request(path: String, params: [String : String], completion: @escaping (Data?, Error?) -> ()) {
         guard let token = authService.token else { return }
-        let params = ["filters": "post, photo"]
-        
         var allParams = params
         allParams["access_token"] = token
         allParams["v"] = API.version
+        let url = self.url(from: path, params: allParams)
+        let request = URLRequest(url: url)
+        let task = createDataTask(from: request, completion: completion)
+        task.resume()
+        print(url)
+    }
+    
+    private func createDataTask(from request: URLRequest, completion: @escaping (Data?, Error?) -> ()) -> URLSessionDataTask {
+        return URLSession.shared.dataTask(with: request, completionHandler: { (data, response, error) in
+            DispatchQueue.main.async {
+                completion(data, error)
+            }
+        })
         
+    }
+    
+    private func url(from path: String, params: [String: String]) -> URL {
+        var components = URLComponents()
         components.scheme = API.scheme
         components.host = API.host
         components.path = API.newsFeed
-        components.queryItems = allParams.map { URLQueryItem(name: $0, value: $1) }
+        components.queryItems = params.map { URLQueryItem(name: $0, value: $1) }
         
-        let url = components.url!
-        print(url)
+        return components.url!
     }
 }
