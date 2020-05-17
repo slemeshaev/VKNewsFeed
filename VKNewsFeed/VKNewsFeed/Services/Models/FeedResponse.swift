aca969083d834e50c228ff78321d4c61d4fdf020
Stanislav <lemeshaevs@gmail.com>
2020-05-17 22:40:43 +0300

    Создал два файла: NetworkDataFetcher и FeedResponse

---
+++ VKNewsFeed/VKNewsFeed/Services/Models/FeedResponse.swift
@@ -0,0 +1,9 @@
+//
+//  FeedResponse.swift
+//  VKNewsFeed
+//
+//  Created by Станислав Лемешаев on 17.05.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
---
+++ VKNewsFeed/VKNewsFeed/Services/NetworkDataFetcher.swift
@@ -0,0 +1,9 @@
+//
+//  NetworkDataFetcher.swift
+//  VKNewsFeed
+//
+//  Created by Станислав Лемешаев on 17.05.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
