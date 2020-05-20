70534d8d400401acbd2d50dcc00aa0028dedda98
Stanislav <lemeshaevs@gmail.com>
2020-05-20 21:00:19 +0300

    Создал два файла: constraints и NewsfeedCodeCell

---
+++ VKNewsFeed/VKNewsFeed/Newsfeed/Cell/NewsfeedCodeCell.swift
@@ -0,0 +1,9 @@
+//
+//  NewsfeedCodeCell.swift
+//  VKNewsFeed
+//
+//  Created by Станислав Лемешаев on 20.05.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
