59b21ae40ebf434d197aab1aa1d18659e45bab44
Stanislav <lemeshaevs@gmail.com>
2020-05-20 14:08:36 +0300

    Создал файл для подсчета ячеек

---
+++ VKNewsFeed/VKNewsFeed/Newsfeed/Cell/NewsfeedCellLayoutCalculator.swift
@@ -0,0 +1,9 @@
+//
+//  NewsfeedCellLayoutCalculator.swift
+//  VKNewsFeed
+//
+//  Created by Станислав Лемешаев on 20.05.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
