8b89f45cd9d84288fcd6a295a2a23e8153c96d07
Stanislav <lemeshaevs@gmail.com>
2020-05-30 02:25:42 +0300

    Добавил файлы для заголовка ленты

---
+++ VKNewsFeed/VKNewsFeed/Newsfeed/TitleView/TitleView.swift
@@ -0,0 +1,9 @@
+//
+//  TitleView.swift
+//  VKNewsFeed
+//
+//  Created by Станислав Лемешаев on 28.05.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
---
+++ VKNewsFeed/VKNewsFeed/Services/Models/UserResponse.swift
@@ -0,0 +1,9 @@
+//
+//  UserResponse.swift
+//  VKNewsFeed
+//
+//  Created by Станислав Лемешаев on 29.05.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
